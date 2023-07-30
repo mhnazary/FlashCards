1e101357b88b3ce74f5fcbb654f6b0683c61aaea
MHNazary <mhnazary2020@gmail.com>
2023-07-30 09:41:16 +0430

    added list to app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,39 @@
 import { useState } from "react";
 import "./App.css";
 
+let flashcard = [
+  {
+    id: 1,
+    question: "first question",
+    answer: "",
+  },
+  {
+    id: 2,
+    question: "second question",
+    answer: "answer 2",
+  },
+  {
+    id: 3,
+    question: "third question",
+    answer: "answer 3",
+  },
+  {
+    id: 4,
+    question: "fourth question",
+    answer: "answer 4",
+  },
+  {
+    id: 5,
+    question: "fifth question",
+    answer: "answer 5",
+  },
+  {
+    id: 6,
+    question: "six question",
+    answer: "answer 6",
+  },
+];
+
 function App() {
 
   return (
